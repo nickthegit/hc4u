14e13e8b30fe033c090f11cf78998ce180d6ef8a
nickthegit <nick@rosielee.co.uk>
2019-12-07 00:33:04 +0000

    install nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Beauty Bar Yeovil',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'The beauty bar is situated upstairs within Tesco extra right in the heart of Yeovil, the perfect location for your convenience.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'https://fonts.googleapis.com/css?family=Bebas+Neue|Open+Sans&display=swap' },
+      { rel: 'stylesheet', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: ['@/assets/scss/_all.scss'],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/style-resources',
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+  ],
+  styleResources: {
+    scss: [
+      './assets/scss/_variables.scss', // use underscore "_" & also file extension ".scss"
+      './assets/scss/_mediaquery.scss'
+    ]
+  },
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,174 @@
+<template>
+  <main>
+    <section id="hero"></section>
+    <article>
+      <img src="/bar_logo.png" alt="Pure Hair logo" />
+      <section>
+        <h1>Welcome to The Beauty Bar, Yeovil</h1>
+        <p>The beauty bar is situated upstairs within Tesco extra right in the heart of Yeovil, the perfect location for your convenience.</p>
+        <a class="btn" href="javascript:void(0)" onclick="saloniq.loader.openIFrame()">Book Online</a>
+        <!-- <a
+          href="https://purehair.mylocalsalon.com/onlinebooking/v7410/Steps/SelectServices.aspx"
+          class="btn"
+        >Book Online Now</a>-->
+      </section>
+      <section id="contact">
+        <a href="tel:+44 1935 434534">01935 434534</a>
+        <address>4, Tesco Stores, Queensway Pl, Yeovil BA20 1DL</address>
+        <section id="socials">
+          <a href="https://www.facebook.com/beautybaryeovil/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="feather feather-facebook"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+            </svg>
+          </a>
+          <a href="https://www.instagram.com/beautybar_yeovil">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="feather feather-instagram"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zM17.5 6.5h.01" />
+            </svg>
+          </a>
+        </section>
+      </section>
+    </article>
+  </main>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    },
+    mounted() {
+      !(function(b, c) {
+        ;(b.saloniqid = '1e7d7624-feb7-4950-a6be-5fbb1498ee39'),
+          (b.saloniqsite = 'https://demowidget.saloniq.co.uk')
+        var d,
+          g,
+          e = 'onlinebookingwidget-loader',
+          f = c.getElementsByTagName('script')[0]
+        c.getElementById(e) ||
+          ((d = c.createElement('script')),
+          (d.id = e),
+          (d.async = !0),
+          (d.src =
+            b.saloniqsite +
+            '/scripts/onlinebookingwidget-loader.js?d=' +
+            Date.now()),
+          (g = f ? f.parentNode : c.getElementsByTagName('head')[0]),
+          g.insertBefore(d, f))
+      })(window, document)
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  main {
+    width: 100%;
+    height: 100%;
+    background: darkcyan;
+    position: fixed;
+    display: grid;
+    grid-template: 100% / 55% 45%;
+    @include breakpoint(tablet) {
+      grid-template: 40% 60% / 100%;
+    }
+    @include breakpoint(mobile) {
+      grid-template: 40% 60% / 100%;
+    }
+  }
+  #hero {
+    background-image: url('/bar_salon_shot.jpg');
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  article {
+    background: $white;
+    padding: 40px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    overflow: scroll;
+    h1 {
+      font-family: 'Bebas Neue', cursive;
+      font-weight: 400;
+      font-size: 36px;
+      padding-bottom: 20px;
+      color: $primary;
+    }
+    p {
+      padding-bottom: 40px;
+      line-height: 20px;
+    }
+    img {
+      width: 200px;
+      padding-bottom: 40px;
+    }
+    section {
+      max-width: 500px;
+      padding-bottom: 40px;
+    }
+    a {
+      font-size: 18px;
+      font-weight: 500;
+      padding-bottom: 10px;
+      display: block;
+      color: $primary;
+    }
+  }
+  #contact {
+    padding-bottom: 0;
+  }
+  #socials {
+    padding-bottom: 0;
+    padding-top: 20px;
+    a {
+      width: 30px;
+      height: 30px;
+      display: inline-block;
+      margin: 0 10px;
+      svg {
+        width: 100%;
+        stroke: $primary;
+      }
+    }
+  }
+  .btn {
+    background: $primary;
+    color: $white;
+    text-decoration: none;
+    padding: 15px 40px;
+    border-radius: 40px;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 500;
+    display: initial;
+    &:hover,
+    &:active,
+    &:focus {
+      opacity: 0.7;
+    }
+  }
+</style>
